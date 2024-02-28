ac40786b7f0234a8160b982521f7a3641cda198e
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-02-29 01:20:27 +0530

    Basic nav bar div structure made

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,13 @@ const Navbar = () => {
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
         <header className="relative bg-white">
-            <MaxWidthWrapper></MaxWidthWrapper>
+            <MaxWidthWrapper>
+                <div className="border-b border-gray-200">
+                    <div className="flex h-16 items-center">
+                        {/* Mobile Navigation */}
+                    </div>
+                </div>
+            </MaxWidthWrapper>
         </header>
     </div>
   )
