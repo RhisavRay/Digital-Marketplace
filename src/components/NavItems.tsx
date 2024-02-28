e2cef719ed756caa0905184df87451e1c3011636
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-02-29 01:41:36 +0530

    use client added

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+"use client" //Added to turn it to a client side component. Bydefault all Next.js components are server side components.
 
 const NavItems = () => {
   return (
-    <div>NavItems</div>
+    
   )
 }
 
