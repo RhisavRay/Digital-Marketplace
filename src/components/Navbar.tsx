d38d85f6bc0852631e042dec1ffe3163c12036c3
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-02-29 01:32:36 +0530

    Logo added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import Link from 'next/link'
+import { Icons } from './Icons'
 
 const Navbar = () => {
   return (
@@ -14,7 +15,7 @@ const Navbar = () => {
                         {/* Desktop Navigation */}
                         <div className="ml-4 flex lg:ml-0">
                             <Link href='/'>
-                                {/* Logo Goes Here */}
+                                <Icons.logo className='h-10 w-10'/>
                             </Link>
                         </div>
                     </div>
