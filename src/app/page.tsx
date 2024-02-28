bfa7af2396c31c3f1ea7610162c399e5014ed2c9
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-02-28 23:38:57 +0530

    h3 tag for perk name added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,9 @@ export default function Home() {
                 </div>
 
                 <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
-                  
+                  <h3 className="text-base font-medium text-gray-900">
+                    {perk.name}
+                  </h3>
                 </div>
               </div>
             ))}
