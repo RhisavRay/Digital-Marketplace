b660f8ab2f4d54e7df008c747d03a9515eb39732
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-02-29 22:32:46 +0530

    activeState useState addde with a descriptive comment new users of TypeScript

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,6 +1,14 @@
-"use client" //Added to turn it to a client side component. Bydefault all Next.js components are server side components.
+"use client"
+import { useState } from "react"
+
+ //Added to turn it to a client side component. Bydefault all Next.js components are server side components.
 
 const NavItems = () => {
+
+  const [activeState, setActiveState] = useState< null | number >(null)
+  // Everything is is the same as using useState while in JS. The only new thing is the content within <>
+  // Since this is TypeScript, we need to define the data types that the variable of the state, i.e., the variable "activeState" for this case, can accept.
+
   return (
     <div className="flex gap-4 h-full"></div>
   )
