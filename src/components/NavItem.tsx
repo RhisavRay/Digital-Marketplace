4301f30ae7502ee9775234d59f9f21bba4f8987a
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-03-01 01:43:48 +0530

    a new interface is created

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,6 +1,20 @@
+import { PRODUCT_CATEGORIES } from "@/config"
+import { Button } from "./ui/button"
+
+interface NavItemProps {
+    category: Category
+    handleOpen: () => void
+    isOpen: boolean
+    isAnyOpen: boolean
+}
+
 const NavItem = () => {
   return (
-    <div>NavItem</div>
+    <div className="flex">
+        <div className="relative flex items-center">
+            <Button className="gap-1.5"></Button>
+        </div>
+    </div>
   )
 }
 
