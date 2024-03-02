3bfb586ebf0615bdaa8a1e4ea6e28ae58869b56a
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-03-02 23:43:01 +0530

    imported separator in Cart.tsx

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -2,6 +2,7 @@
 
 import { ShoppingCart } from "lucide-react"
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet"
+import { Separator } from "./ui/separator"
 
 const Cart = () => {
 
