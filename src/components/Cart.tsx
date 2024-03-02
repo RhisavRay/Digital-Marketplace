f5411cfb7efa1620b244d5a381367202f393c1b2
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-03-02 23:29:20 +0530

    SheetTrigger is made

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,10 +1,19 @@
 "use client"
 
-import { Sheet } from "./ui/sheet"
+import { ShoppingCart } from "lucide-react"
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
 
 const Cart = () => {
   return (
-    <Sheet></Sheet>
+    <Sheet>
+      <SheetTrigger className="group -m-2 flex items-center p-2">
+        <ShoppingCart className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" aria-hidden="true"/>
+
+        <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+          0
+        </span>
+      </SheetTrigger>
+    </Sheet>
   )
 }
 
