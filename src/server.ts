3a8d148e1986ec8581cd4f49cfeb74d7d2f16e6b
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-03-05 03:59:05 +0530

    Used a getPayloadClient function

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const PORT = Number(process.env.PORT) || 3000
 // Says that the port will be 3000 by default in development mode, and a port for production will be given later on
 
 const start = async() => {
-    
+    const payload = await getPayloadClient()
 }
 
 start()
