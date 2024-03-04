0496940cb884df6a62e741589ec3c5721cbfeddd
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-03-05 04:06:48 +0530

    Set cached

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -5,6 +5,16 @@ dotenv.config({
     path: path.resolve(__dirname, "../.env")
 })
 
+let cached = (global as any).payload
+
+if(!cached)
+{
+    cached = (global as any).payload = {
+        client: null,
+        promise: null
+    }
+}
+
 export const getPayloadClient = async() => {
 
 }
