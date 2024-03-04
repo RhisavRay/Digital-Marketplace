b31d72465c0765616fb9c0b80c7d08c30954d357
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-03-05 04:21:29 +0530

    .

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,14 @@ const PORT = Number(process.env.PORT) || 3000
 // Says that the port will be 3000 by default in development mode, and a port for production will be given later on
 
 const start = async() => {
-    const payload = await getPayloadClient()
+    const payload = await getPayloadClient({
+        initOptions: {
+            express: app,
+            onInit: async(cms) => {
+                cms.logger.info(`Admin URL ${cms.getAdminURL()}`)
+            }
+        }
+    })
 }
 
 start()
