5593fd7e8deda926a7fbb10c42a1c0d17cf4e54f
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-03-05 03:57:17 +0530

    add start function and have it called at the end

--- src/server.ts
+++ src/server.ts
@@ -5,3 +5,8 @@ const app = express()
 const PORT = Number(process.env.PORT) || 3000
 // Says that the port will be 3000 by default in development mode, and a port for production will be given later on
 
+const start = async() => {
+    
+}
+
+start()
