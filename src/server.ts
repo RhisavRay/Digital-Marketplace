050d606f2eb0546d8a3de4f3c5cb201bfb7181a3
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-03-06 02:37:10 +0530

    For console checking purpose only. To fix bugs according to our console output

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express, { request } from "express"
 import { getPayloadClient } from "./get-payload"
-import { nextHandler } from "./next-utils"
+import { nextApp, nextHandler } from "./next-utils"
 
 const app = express()
 
@@ -18,6 +18,14 @@ const start = async() => {
     })
 
     app.use((req, res) => nextHandler(req, res))
+
+    nextApp.prepare().then(() => {
+        payload.logger.info("Next.js started")
+
+        app.listen(PORT, async() => {
+            payload.logger.info(`Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`)
+        })
+    })
 }
 
 start()
