ecfe1c8d3ac8b3cefbb6c69c734117a3ba02436c
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-03-07 04:57:04 +0530

    A button to redirect to the sign in page

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,4 +1,6 @@
 import { Icons } from "@/components/Icons"
+import { buttonVariants } from "@/components/ui/button"
+import Link from "next/link"
 import React from "react"
 
 const Page = () => {
@@ -8,6 +10,14 @@ const Page = () => {
                 <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
                     <div className="flex flex-col items-center space-y-2 text-center">
                         <Icons.logo className="h-20 w-20"/>
+
+                        <h1 className="text-2xl font-bold"> Create an account </h1>
+
+                        <Link href='/sign-in' className={buttonVariants({
+                            variant: "link"
+                        })}>
+                            Already have an account with us?
+                        </Link>
                     </div>
                 </div>
             </div>
