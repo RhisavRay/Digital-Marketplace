5b79bc41199ff60e2e2c19820cc478385dabf7d9
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-03-11 17:40:23 +0530

    .

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,9 +2,19 @@
 
 import { useState } from "react"
 import { QueryClient } from "@tanstack/react-query"
+import { trpc } from "@/trpc/client"
+import { httpBatchLink } from "@trpc/client"
 
 const Providers = () => {
-    const [queryClient] = useState(() => new QueryClient)
+    const [queryClient] = useState(() => new QueryClient())
+
+    const [trpcClient] = useState(() => trpc.createClient({
+        links: [
+            httpBatchLink({
+                url: `${process.env.}`
+            })
+        ]
+    }))
 }
 
 export default Providers
