f4947f26f86debf3f4efd0f1cf31480322482db3
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-03-11 16:07:08 +0530

    import z and export the validator. Minor name change of file

--- src/lib/validators/account-credentials-validator.ts
+++ src/lib/validators/account-credentials-validator.ts
@@ -1,4 +1,6 @@
-const AuthCredentialValidator = z.object({
+import { z } from "zod"
+
+export const AuthCredentialValidator = z.object({
     email: z.string().email(),
     password: z.string()
         .min(8, {
