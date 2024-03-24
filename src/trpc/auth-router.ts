c370cf26bf2220da47bbab1ea8df1ed593d002f0
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-03-25 00:32:53 +0530

    .

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -1,6 +1,6 @@
-import { AuthCredentialValidator } from "@/lib/validators/account-credentials-validator";
+import { AuthCredentialValidator } from "../lib/validators/account-credentials-validator";
 import { publicProcedure, router } from "./trpc";
-import { getPayloadClient } from "@/get-payload";
+import { getPayloadClient } from "../get-payload";
 import { TRPCError } from "@trpc/server";
 
 export const authRouter = router({
