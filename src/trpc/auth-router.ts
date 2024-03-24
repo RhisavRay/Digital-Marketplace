c17a61e1d9efa99184500412c8704ef9ba0f35cb
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-03-25 00:01:23 +0530

    mutate input as per requirement

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -1,6 +1,12 @@
 import { AuthCredentialValidator } from "@/lib/validators/account-credentials-validator";
 import { publicProcedure, router } from "./trpc";
+import { getPayloadClient } from "@/get-payload";
 
 export const authRouter = router({
-    createPayloadUser: publicProcedure.input(AuthCredentialValidator)
+    createPayloadUser: publicProcedure.input(AuthCredentialValidator).mutation(async ({input}) => {
+        const {email, password} = input
+        const payload = await getPayloadClient()
+        
+        // Check if user already exists
+    })
 })
