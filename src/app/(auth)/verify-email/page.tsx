5fbe83ef5c332eb4b88135be80515c46903189d0
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-04-06 01:00:54 +0530

    .

--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -15,7 +15,9 @@ const VerifyEmailPage = ({ searchParams }: PageProps) => {
         <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
             <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
                 { token && typeof token === "string" ? (
-                    <div className="grid gap-6"></div>
+                    <div className="grid gap-6">
+                        {/* A verify Email client side component is to be added */}
+                    </div>
                 ) : (
                     <div className="flex h-full flex-col items-center justify-center space-y-1">
                         <div className="relative mb-4 h-16 w-16 text-muted-foreground">
